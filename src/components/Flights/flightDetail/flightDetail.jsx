9f31a0d45182600893ed10e4e2abbfd7d73a168b
Nicolain12 <109926340+Nicolain12@users.noreply.github.com>
2023-05-27 14:08:55 -0400

    flight detail started

--- src/components/Flights/flightDetail/flightDetail.jsx
+++ src/components/Flights/flightDetail/flightDetail.jsx
@@ -1,54 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './flightDetail.css';
+import { useParams } from 'react-router-dom';
 
 function FlightDetail() {
+    const { id } = useParams();
+    const [flight, setFlight] = useState({});
+    const [flightImgs, setFlightImgs] = useState([]);
+    async function fetchApi(endpoint, config) {
+        try {
+            const response = await fetch(endpoint, config);
+            const jsonResponse = await response.json();
+            if (jsonResponse.info.status === 200) {
+                return jsonResponse
+
+            }
+            if (jsonResponse.info.status === 400) {
+                console.log(jsonResponse);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const refSliderDiv = useRef()
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const headers = {};
+                const permanentToken = localStorage.getItem('token');
+                const token = sessionStorage.getItem('token');
+                if (token) headers.authorization = token;
+                if (permanentToken) headers.authorization = permanentToken;
+                const flightData = await fetchApi(`http://localhost:3001/api/products/flight/${id}`, {
+                    method: 'GET',
+                    headers,
+                });
+                setFlight(flightData.data);
+                setFlightImgs(flightData.data.image)
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    console.log(flight);
+    console.log(flightImgs);
+
     return (
         <div className="App-flightDetail">
-               <main className="main-detail-flight">
-                <section className="section-info">
+            <main className="fd-main-detail-flight">
+                <section className="fd-section-info">
 
-                    <div className="carousel_detail-flight">
-                        <img src="/images/flights/495536-lasvegas.webp" alt=""></img>
+                    <div id="slider-root" ref={refSliderDiv} className="fd-carousel_detail-flight">
+                        {/********************** SLIDER HERE **********************/}
                     </div>
 
-                    <div className="detail-flight-info">
-                        <div className="detail-flight-info-maininfo">
-                            <div className="detail-flight-info-header">
-                                <h3>Departure ----- Reach</h3>
+                    <div className="fd-detail-flight-info">
+                        <div className="fd-detail-flight-info-maininfo">
+                            <div className="fd-detail-flight-info-header">
+                                <h3>{flight.departure} ----- {flight.reach}</h3>
                             </div>
-                            <div className="detail-flight-info-data">
-                                <div className="detail-flight-departure">
-                                    <h4 className="detail-flight-h4">Salida:</h4>
-                                    <p className="detail-flight-date">Date</p>
-                                    <p className="detail-flight-hour">Hour</p>
+                            <div className="fd-detail-flight-info-data">
+                                <div className="fd-detail-flight-departure">
+                                    <h4 className="fd-detail-flight-h4">Departure Day Info:</h4>
+                                    <p className="fd-detail-flight-date">{flight.departure_date}</p>
+                                    <p className="fd-detail-flight-hour">{flight.departure_hour}</p>
                                 </div>
-                                <div className="detail-flight-reach">
-                                    <h4 className="detail-flight-h4">Llegada:</h4>
-                                    <p className="detail-flight-date">Date</p>
-                                    <p className="detail-flight-hour">Hour</p>
+                                <div className="fd-detail-flight-reach">
+                                    <h4 className="fd-detail-flight-h4">Return Day Info:</h4>
+                                    <p className="fd-detail-flight-date">{flight.reach_date}</p>
+                                    <p className="fd-detail-flight-hour">{flight.reach_hour}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="detail-flight-info">
-                        <div className="detail-flight-info-title">
+                    <div className="fd-detail-flight-info">
+                        <div className="fd-detail-flight-info-title">
                             <h3>Informacion de la aerolinea</h3>
                         </div>
 
-                        <div className="detail-flight-info-maininfo">
-                            <div className="detail-flight-info-header">
-                                <h3>Airline: "Airline"</h3>
+                        <div className="fd-detail-flight-info-maininfo">
+                            <div className="fd-detail-flight-info-header">
+                                <h3>Airline: {flight.airline}</h3>
                             </div>
-                            <div className="detail-flight-info-data">
-                                <div className="detail-flight-departure">
-                                    <h5>Cabina: "Tal Cabina"</h5>
-                                    <div className="detail-flight-description">
+                            <div className="fd-detail-flight-info-data">
+                                <div className="fd-detail-flight-departure">
+                                    <h5>Cabina: {flight.cabin}</h5>
+                                    <div className="fd-detail-flight-description">
                                         <h5>Descripcion</h5>
-                                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                            Officiis minima maxime voluptatibus, magni delectus culpa, assumenda eum m
-                                            inus deserunt enim nulla, quam debitis modi et perspiciatis sint consequatur
-                                            sapiente praesentium.</p>
+                                        <p>{flight.description}</p>
                                     </div>
                                 </div>
                             </div>
@@ -57,12 +100,12 @@ function FlightDetail() {
 
                 </section>
 
-                <section className="detail-flight-button-section">
-                    <button className="detail-flight-button btn-df-edit">Editar</button>
-                    <button className="detail-flight-button btn-df-delete">Eliminar</button>
-                    <div className="detail-flight-button-buy">
+                <section className="fd-detail-flight-button-section">
+                    <button className="fd-detail-flight-button btn-df-edit">Editar</button>
+                    <button className="fd-detail-flight-button btn-df-delete">Eliminar</button>
+                    <div className="fd-detail-flight-button-buy">
                         <p>Price: $999999</p>
-                        <button className="detail-flight-button btn-df-buy">Comprar</button>
+                        <button className="fd-detail-flight-button btn-df-buy">Comprar</button>
                     </div>
                 </section>
             </main>
